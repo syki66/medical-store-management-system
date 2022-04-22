d96df532fc7f560702db9afce515fad5fe9ddbcc
Soyeon Jang <ssso.pro1@gmail.com>
2022-04-22 14:20:48 +0900

    style : add reset.css & GlobalStyle.js
    - reset.css 추가
    - GlobalStyle.js 추가 -> 제목이나 글씨체 등 공통적으로 필요한 css 여기에 추가해서 통일하면 될 것 같습니다

--- Client/landing_project/src/App.js
+++ Client/landing_project/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Header from './components/Header'
 import SideMenuBar from "./components/SideMenuBar";
 import Button from '@mui/material/Button';
-
+import GlobalStyle from './components/GlobalStyle'
 
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <div className="container">
           <SideMenuBar />
