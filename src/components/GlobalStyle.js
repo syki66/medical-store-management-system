4d73c483332faadca9aace686507ae3c0ee3f6cb
Soyeon Jang <ssso.pro1@gmail.com>
2022-04-28 13:44:42 +0900

    Install some packages
    - source-map-loader
    - devexpress/dx-react-chart (Home 페이지 차트)
    - material-ui/core
    - react-cookies
    - react-icons
    - styled-reset : reset.css 삭제하고 styled-reset 사용

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -1,7 +1,8 @@
 import { createGlobalStyle } from 'styled-components'
-import './reset.css'
+import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
+  ${reset}
   h3 {
     font-size: 1.5rem;
     margin-bottom: 2rem;
