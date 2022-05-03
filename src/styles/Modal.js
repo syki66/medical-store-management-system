b59615346d1d0b01df108b77f2054031ce0a66ab
syki66 <66syki@gmail.com>
2022-05-03 16:18:25 +0900

    fix: modal.js
    - 일부 styled-components가 새로고침 이후 적용되는 문제 해결

--- src/styles/Modal.js
+++ src/styles/Modal.js
@@ -1,12 +1,15 @@
 import styled from 'styled-components';
 import { Box, Grid } from "@mui/material";
+import React from "react";
 
 const gap = '17px 0px';
 
 const ModalContainer = styled(Box)`
-  background-color: #d9d9d9;
-  padding: 10px 20px 10px 20px;
-  border-radius: 5px;
+  && {
+    background-color: #d9d9d9;
+    padding: 10px 20px 10px 20px;
+    border-radius: 5px;
+  }
 `;
 
 const CloseButton = styled.div`
@@ -15,31 +18,41 @@ const CloseButton = styled.div`
 `;
 
 const GridContainer = styled(Grid)`
-  background-color: #FFFFFF;
-  padding: 10px;
-  width: 700px;
-  border-radius: 5px;
+  && {
+    background-color: #FFFFFF;
+    padding: 10px;
+    width: 700px;
+    border-radius: 5px;
+  }
 `;
 
 const GridDescription = styled(Grid)`
-  overflow-y: auto;
-  min-height: 40px;
-  max-height: 200px;
+  && {
+    overflow-y: auto;
+    min-height: 40px;
+    max-height: 200px;
+  }
 `;
 
 const GridInnerTitle = styled(Grid)`
-  font-size: x-large;
-  font-weight: 600;
-  margin: ${gap};
+  && {
+    font-size: x-large;
+    font-weight: 600;
+    margin: ${gap};
+  }
 `;
 
 const GridName = styled(Grid)`
-  font-weight: 600;
-  margin: ${gap};
+  && {
+    font-weight: 600;
+    margin: ${gap};
+  }
 `;
 
 const GridContent = styled(Grid)`
-  margin: ${gap};
+  && {
+    margin: ${gap};
+  }
 `;
 
 const StyledButton = styled.div`
