4504150a293c2c1b97e4f0b417963337421bd6e2
syki66 <66syki@gmail.com>
2022-05-18 17:30:24 +0900

    Update package.json

--- src/variables/baseURL.js
+++ src/variables/baseURL.js
@@ -2,10 +2,10 @@
 
 // export const baseURL = 'http://172.31.0.197:8000/';
 
-export const baseURL = 'http://localhost:8000/';
+// export const baseURL = 'http://localhost:8000/';
 
 // export const baseURL = 'http://3.36.26.172:8000/';
 
-// export const baseURL = 'https://hms.imtrial.com:8000';
-
 // export const baseURL = 'http://3.39.240.194:8000';
+
+export const baseURL = 'https://hms.imtrial.com:8000/';
